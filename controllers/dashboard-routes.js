4d81c82ce311698597c9f60eba799690c1930ce0
S317 <seifenan@gmail.com>
2021-12-21 23:20:33 -0500

    modified controllers, models, add&edit posts.js, hbs & package.json

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -13,7 +13,7 @@ router.get('/', withAuth, (req, res) => {
     },
     attributes: [
       'id',
-      'post_url',
+      'content',
       'title',
       'created_at'
     ],
@@ -46,7 +46,7 @@ router.get('/edit/:id', withAuth, (req, res) => {
   Post.findByPk(req.params.id, {
     attributes: [
       'id',
-      'post_url',
+      'content',
       'title',
       'created_at'
     ],
