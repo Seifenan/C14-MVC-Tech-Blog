88f212b3e80d253784f62183468ea063c5d71a29
S317 <seifenan@gmail.com>
2021-12-22 02:10:11 -0500

    testing heroku deployment

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   Post.findAll({
     attributes: [
       'id',
-      'post_url',
+      'content',
       'title',
       'created_at'
     ],
@@ -49,7 +49,7 @@ router.get('/post/:id', (req, res) => {
     },
     attributes: [
       'id',
-      'post_url',
+      'content',
       'title',
       'created_at'
     ],
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,12 +2,13 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value.trim();
-  const content = document.querySelector('input[name="post-url"]').value.trim();
+  const content = document.querySelector('input[name="content"]').value.trim();
 
   
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
+
   const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
