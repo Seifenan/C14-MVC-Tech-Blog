1ba4eb5791422eeee947075a2c1b8f1effbd9b7a
S317 <seifenan@gmail.com>
2021-12-22 04:23:21 -0500

    cleaned up formatting

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -59,7 +59,7 @@ router.post('/', (req, res) => {
         req.session.user_id = dbUserData.id;
         req.session.username = dbUserData.username;
         req.session.loggedIn = true;
-  
+
         res.json(dbUserData);
       });
     })
@@ -92,7 +92,7 @@ router.post('/login', (req, res) => {
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
-  
+
       res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
   });
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -68,7 +68,7 @@ router.get('/edit/:id', withAuth, (req, res) => {
     .then(dbPostData => {
       if (dbPostData) {
         const post = dbPostData.get({ plain: true });
-        
+
         res.render('edit-post', {
           post,
           loggedIn: true
